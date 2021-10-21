a27828396e60030777c9168f91cd902f2f61fb13
Leonardo Gardini <leogardini10.1@gmail.com>
2021-10-21 01:27:52 -0300

    Added the DELETE and PUT routes

--- controllers/book_controller.go
+++ controllers/book_controller.go
@@ -8,6 +8,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func ListBooks(c *gin.Context) {
+	db := db.GetDatabase()
+
+	var books []models.Book
+	err := db.Find(&books).Error
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Cannot list books (" + err.Error() + ")"})
+		return
+	}
+
+	c.JSON(200, books)
+}
+
 func ShowBook(c *gin.Context) {
 	id := c.Param("id")
 	newId, err := strconv.Atoi(id)
@@ -48,15 +61,46 @@ func CreateBook(c *gin.Context) {
 	c.JSON(201, book)
 }
 
-func ListBooks(c *gin.Context) {
+func UpdateBook(c *gin.Context) {
 	db := db.GetDatabase()
 
-	var books []models.Book
-	err := db.Find(&books).Error
+	var book models.Book
+	err := c.ShouldBindJSON(&book)
 	if err != nil {
-		c.JSON(400, gin.H{"error": "Cannot list books (" + err.Error() + ")"})
+		c.JSON(400, gin.H{"error": "Cannot find JSON (" + err.Error() + ")"})
 		return
 	}
 
-	c.JSON(200, books)
+	err = db.Save(&book).Error
+	if err != nil {
+		return
+	}
+
+	c.JSON(201, book)
+}
+
+func DeleteBook(c *gin.Context) {
+	id := c.Param("id")
+	newId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "ID has to be integer"})
+		return
+	}
+
+	db := db.GetDatabase()
+
+	var book models.Book
+	err = db.Find(&book).Error
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Cannot find book (" + err.Error() + ")"})
+		return
+	}
+
+	err = db.Delete(book, newId).Error
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Cannot delete book (" + err.Error() + ")"})
+		return
+	}
+
+	c.JSON(200, book)
 }
